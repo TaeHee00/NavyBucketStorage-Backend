36e8104e58d2e8020c950c3f3cfaeda972c7995a
Tae Hee Jang <kancth03@gmail.com>
2024-11-15 00:42:03 +0900

    hotfix: controller request body change

--- src/main/java/com/kancth/navybucketstorage/domain/user/controller/UserController.java
+++ src/main/java/com/kancth/navybucketstorage/domain/user/controller/UserController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<RegisterResponse> register(RegisterRequest request) {
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                              .body(RegisterResponse.of(userService.register(request)));
     }
