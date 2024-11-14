2f64454dfc13ee417b04f000eafa2e0cb954726f
Tae Hee Jang <kancth03@gmail.com>
2024-11-15 00:42:31 +0900

    fix: add notnull field

--- src/main/java/com/kancth/navybucketstorage/domain/user/entity/User.java
+++ src/main/java/com/kancth/navybucketstorage/domain/user/entity/User.java
@@ -27,8 +27,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String username;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
 
     @CreatedDate
