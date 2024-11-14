2bfedb27c26dd7635e50e981aa59f66c391ceb3b
Tae Hee Jang <kancth03@gmail.com>
2024-11-14 21:36:06 +0900

    setting: spring boot init

---
+++ src/main/java/com/kancth/navybucketstorage/NavyBucketStorageApplication.java
@@ -0,0 +1,13 @@
+package com.kancth.navybucketstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavyBucketStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavyBucketStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kancth/navybucketstorage/NavyBucketStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kancth.navybucketstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavyBucketStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
