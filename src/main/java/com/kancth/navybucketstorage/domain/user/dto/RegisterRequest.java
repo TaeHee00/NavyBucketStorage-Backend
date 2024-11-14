a5ad4ef2b31595a4691d5903f95f8ed39cb5c325
Tae Hee Jang <kancth03@gmail.com>
2024-11-15 00:41:24 +0900

    Fix: add Request email filed validation

--- src/main/java/com/kancth/navybucketstorage/domain/user/dto/RegisterRequest.java
+++ src/main/java/com/kancth/navybucketstorage/domain/user/dto/RegisterRequest.java
@@ -1,9 +1,10 @@
 package com.kancth.navybucketstorage.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterRequest(
-        @NotBlank(message = "username이 입력되지 않았습니다.") String username,
+        @NotBlank(message = "username이 입력되지 않았습니다.") @Email(message = "이메일 형식이 올바르지 않습니다.") String username,
         @NotBlank(message = "email이 입력되지 않았습니다.") String email,
         @NotBlank(message = "password가 입력되지 않았습니다.") String password
 ) {
