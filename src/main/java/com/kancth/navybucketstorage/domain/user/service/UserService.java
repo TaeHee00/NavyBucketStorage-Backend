5a42d20c103a51921c23bc14064ae54e268a56ca
Tae Hee Jang <kancth03@gmail.com>
2024-11-16 01:44:22 +0900

    feat: Password 암호화 기능 추가

--- src/main/java/com/kancth/navybucketstorage/domain/user/service/UserService.java
+++ src/main/java/com/kancth/navybucketstorage/domain/user/service/UserService.java
@@ -5,18 +5,23 @@ import com.kancth.navybucketstorage.domain.user.entity.User;
 import com.kancth.navybucketstorage.domain.user.exception.UserAlreadyExistsException;
 import com.kancth.navybucketstorage.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     public User register(RegisterRequest request) {
         checkEmailDuplicate(request.email());
 
-        return userRepository.save(User.create(request));
+        User user = User.create(request);
+        user.setPassword(passwordEncoder.encode(request.password()));
+
+        return userRepository.save(user);
     }
 
     private void checkEmailDuplicate(String email) {
