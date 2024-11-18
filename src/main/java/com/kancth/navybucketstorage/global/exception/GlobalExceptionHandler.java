3aff436cf3471700951b621a7ca6f9635dbe237c
Tae Hee Jang <kancth03@gmail.com>
2024-11-19 00:44:05 +0900

    fix: Message 내부 record 삭제

--- src/main/java/com/kancth/navybucketstorage/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kancth/navybucketstorage/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.kancth.navybucketstorage.global.exception;
 
-import lombok.Builder;
+import com.kancth.navybucketstorage.global.exception.dto.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,19 +12,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = BaseException.class)
     public ResponseEntity<Message> globalExceptionHandler(BaseException e) {
         log.error("On Exception: {}, HttpStatus: {}", e.getMessage(), e.getHttpStatus());
-
         return ResponseEntity.status(e.getHttpStatus())
                              .body(Message.of(e.getMessage()));
-    }
-}
-
-@Builder
-record Message(
-        String message
-) {
-    public static Message of(String message) {
-        return Message.builder()
-                    .message(message)
-                    .build();
     }
 }
