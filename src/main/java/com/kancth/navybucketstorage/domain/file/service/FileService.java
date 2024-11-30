083f1a1e7dcec446b81c1fe49edd2684fda5028e
Tae Hee Jang <kancth03@gmail.com>
2024-11-30 23:27:01 +0900

    refactor: FileService Refactoring

--- src/main/java/com/kancth/navybucketstorage/domain/file/service/FileService.java
+++ src/main/java/com/kancth/navybucketstorage/domain/file/service/FileService.java
@@ -11,6 +11,7 @@ import com.kancth.navybucketstorage.domain.user.entity.User;
 import com.kancth.navybucketstorage.global.exception.UnauthorizedAccessException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,6 +27,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class FileService {
@@ -40,7 +42,7 @@ public class FileService {
 
         // bucket owner check
         this.checkBucketOwner(bucket, user);
-        // File name 중복 확인
+        // File name 중복 확인 -> 버킷에 같은 이름을 가진 file이 있을 경우 업로드X
         Map<Boolean, List<MultipartFile>> duplicateCheckFileList = this.checkDuplicateFileList(bucket, createFileRequest.files());
         List<MultipartFile> duplicateList = new ArrayList<>();
         if (duplicateCheckFileList.get(true) != null) {
@@ -52,10 +54,16 @@ public class FileService {
         if (duplicateCheckFileList.get(false) != null) {
             successedFileList.addAll(fileRepository.saveAll(duplicateCheckFileList.get(false).stream().map((file) -> File.create(bucket, file)).toList()));
         }
-        Path path = Paths.get("/Users/mac/Desktop/navy-cloud");
 
+        List<EntityAndFile> entityAndFiles = translateEntityAndFile(successedFileList, createFileRequest);
+        saveFile(entityAndFiles);
+
+        return CreateFileListResponse.of(bucket, successedFileList, failedFileList);
+    }
+
+    private List<EntityAndFile> translateEntityAndFile(List<File> fileList, CreateFileRequest createFileRequest) {
         List<EntityAndFile> entityAndFiles = new ArrayList<>();
-        successedFileList.forEach((file) -> {
+        fileList.forEach((file) -> {
             Stream.of(createFileRequest.files()).forEach((multipartFile) -> {
                 if (multipartFile.getOriginalFilename().equals(file.getFileName())) {
                     entityAndFiles.add(new EntityAndFile(file, multipartFile));
@@ -63,16 +71,7 @@ public class FileService {
             });
         });
 
-        entityAndFiles.forEach((file) -> {
-            try {
-                Path destinationFile = path.resolve(file.entity.getFileName());
-                Files.copy(file.file().getInputStream(), destinationFile, StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        return CreateFileListResponse.of(bucket, successedFileList, failedFileList);
+        return entityAndFiles;
     }
 
     private void checkBucketOwner(Bucket bucket, User user) {
@@ -86,8 +85,35 @@ public class FileService {
                 .collect(Collectors.groupingBy((file) -> fileRepository.existsByBucketAndFileName(bucket, file.getOriginalFilename())));
     }
 
-    record EntityAndFile (
+    private void createFolder(String path) {
+        java.io.File folder = new java.io.File(path);
+        if (!folder.exists()) {
+            if (folder.mkdirs()) {
+                System.out.println("폴더가 생성되었습니다: " + path);
+            } else {
+                log.error("[NCP] Security Error - 권한 문제로 인하여 폴더 생성에 실패하였습니다. error: {}", path);
+            }
+        }
+    }
+
+    private void saveFile(List<EntityAndFile> entityAndFiles) {
+        entityAndFiles.forEach((file) -> {
+            try {
+                final String savePath = "/Users/mac/Desktop/navy-cloud/" + file.entity.getPath();
+
+                createFolder(savePath);
+                Path path = Paths.get(savePath);
+                Path destinationFile = path.resolve(file.entity.getFileName());
+                Files.copy(file.file().getInputStream(), destinationFile, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private record EntityAndFile (
             File entity,
             MultipartFile file
     ) {}
 }
+
