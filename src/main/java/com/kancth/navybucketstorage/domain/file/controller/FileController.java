b4f4a32382b965bd16c041acba02208ffdc5b299
Tae Hee Jang <kancth03@gmail.com>
2024-12-01 00:32:18 +0900

    feat: file download controller 구현
    header에 한국어가 들어가있을 경우 unicode 오류가 발생하여 UTF-8로 인코딩 처리

--- src/main/java/com/kancth/navybucketstorage/domain/file/controller/FileController.java
+++ src/main/java/com/kancth/navybucketstorage/domain/file/controller/FileController.java
@@ -7,9 +7,18 @@ import com.kancth.navybucketstorage.global.interceptor.auth.Auth;
 import com.kancth.navybucketstorage.global.interceptor.auth.AuthType;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/files")
@@ -23,5 +32,22 @@ public class FileController {
     public ResponseEntity<CreateFileListResponse> upload(@ModelAttribute CreateFileRequest createFileRequest, HttpServletRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                              .body(fileService.create(createFileRequest, request));
+    }
+
+    @GetMapping("/{bucketName}/{fileName}")
+    public ResponseEntity<Resource> download(@PathVariable String bucketName, @PathVariable String fileName) {
+        Resource resource = fileService.download(bucketName, fileName);
+
+        String contentType;
+        try {
+            contentType = Files.probeContentType(resource.getFile().toPath());
+        } catch (IOException e) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + URLEncoder.encode(resource.getFilename(), StandardCharsets.UTF_8) + "\"")
+                .body(resource);
     }
 }
