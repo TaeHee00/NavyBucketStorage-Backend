db3e7d884b42e043bdeeef5bb38220073dddba90
Tae Hee Jang <kancth03@gmail.com>
2024-12-01 13:54:53 +0900

    fix: DTO 변경 이후 로직 변경

--- src/main/java/com/kancth/navybucketstorage/domain/bucket/controller/BucketController.java
+++ src/main/java/com/kancth/navybucketstorage/domain/bucket/controller/BucketController.java
@@ -1,6 +1,7 @@
 package com.kancth.navybucketstorage.domain.bucket.controller;
 
 import com.kancth.navybucketstorage.domain.bucket.dto.BucketResponse;
+import com.kancth.navybucketstorage.domain.bucket.dto.CreateBucketResponse;
 import com.kancth.navybucketstorage.domain.bucket.dto.CreateBucketRequest;
 import com.kancth.navybucketstorage.domain.bucket.service.BucketService;
 import com.kancth.navybucketstorage.global.interceptor.auth.Auth;
@@ -9,10 +10,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/buckets")
@@ -23,8 +23,16 @@ public class BucketController {
 
     @PostMapping
     @Auth(authType = AuthType.USER)
-    public ResponseEntity<BucketResponse> create(@RequestBody CreateBucketRequest createBucketRequest, HttpServletRequest request) {
+    public ResponseEntity<CreateBucketResponse> create(@RequestBody CreateBucketRequest createBucketRequest, HttpServletRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
-                             .body(BucketResponse.of(bucketService.create(createBucketRequest, request)));
+                             .body(CreateBucketResponse.of(bucketService.create(createBucketRequest, request)));
     }
+
+    // TODO: User 버킷 목록 가져오기
+    @GetMapping
+    @Auth(authType = AuthType.USER)
+    public ResponseEntity<List<BucketResponse>> list(HttpServletRequest request) {
+        return ResponseEntity.ok(bucketService.list(request).stream().map(BucketResponse::of).toList());
+    }
+    // TODO: 버킷 삭제하기
 }
--- src/main/java/com/kancth/navybucketstorage/domain/file/dto/CreateFileListResponse.java
+++ src/main/java/com/kancth/navybucketstorage/domain/file/dto/CreateFileListResponse.java
@@ -1,23 +1,22 @@
 package com.kancth.navybucketstorage.domain.file.dto;
 
-import com.kancth.navybucketstorage.domain.bucket.dto.BucketResponse;
+import com.kancth.navybucketstorage.domain.bucket.dto.CreateBucketResponse;
 import com.kancth.navybucketstorage.domain.bucket.entity.Bucket;
 import com.kancth.navybucketstorage.domain.file.entity.File;
 import lombok.AccessLevel;
 import lombok.Builder;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record CreateFileListResponse(
-        BucketResponse bucket,
+        CreateBucketResponse bucket,
         List<FileResponse> successedFileList,
         List<String> failedFileList
 ) {
     public static CreateFileListResponse of(Bucket bucket, List<File> successedFileList, List<String> failedFileList) {
         return CreateFileListResponse.builder()
-                .bucket(BucketResponse.of(bucket))
+                .bucket(CreateBucketResponse.of(bucket))
                 .successedFileList(successedFileList.stream().map(FileResponse::of).toList())
                 .failedFileList(failedFileList)
                 .build();
