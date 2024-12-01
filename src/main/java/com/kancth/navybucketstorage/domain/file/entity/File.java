ccb7ef2834dde72996bd316255e7c81170c3e15c
Tae Hee Jang <kancth03@gmail.com>
2024-12-01 13:38:51 +0900

    fix: File > bucket column LAZY Loading으로 변경

--- src/main/java/com/kancth/navybucketstorage/domain/file/entity/File.java
+++ src/main/java/com/kancth/navybucketstorage/domain/file/entity/File.java
@@ -28,7 +28,7 @@ public class File {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "bucket_id", nullable = false)
     private Bucket bucket;
 
