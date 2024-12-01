3689118c46fbd3beb4f3c02ec4472d66a79b6e11
Tae Hee Jang <kancth03@gmail.com>
2024-12-01 14:33:02 +0900

    feat: Bucket Files Query Method 추가

--- src/main/java/com/kancth/navybucketstorage/domain/file/repository/FileRepository.java
+++ src/main/java/com/kancth/navybucketstorage/domain/file/repository/FileRepository.java
@@ -5,6 +5,7 @@ import com.kancth.navybucketstorage.domain.file.entity.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,6 @@ public interface FileRepository extends JpaRepository<File, Long> {
     boolean existsByBucketAndFileName(Bucket bucket, String fileName);
 
     Optional<File> findByUrl(String url);
+
+    List<File> findAllByBucket(Bucket bucket);
 }
