086321cffb6ad80d7acd016b0ea0b1513a6b06e9
Tae Hee Jang <kancth03@gmail.com>
2024-12-02 22:30:03 +0900

    feat: presigned upload API 구현

--- src/main/java/com/kancth/navybucketstorage/domain/file/controller/FileController.java
+++ src/main/java/com/kancth/navybucketstorage/domain/file/controller/FileController.java
@@ -13,9 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -32,7 +32,7 @@ public class FileController {
     public ResponseEntity<CreateFileListResponse> upload(@ModelAttribute CreateFileRequest createFileRequest, HttpServletRequest request) {
         // TODO: 같은 이름으로 된 파일 업로드 시 덮어쓰기 -> 객체 버전관리 기능 만들어서 실수 삭제 or 덮어쓰기 실수 방지
         return ResponseEntity.status(HttpStatus.CREATED)
-                             .body(fileService.create(createFileRequest, request));
+                             .body(fileService.ownerUpload(createFileRequest, request));
     }
 
     @GetMapping("/{bucketName}/{fileName}")
@@ -52,12 +52,25 @@ public class FileController {
                 .body(resource);
     }
 
-    // file ID로 할 경우 History로 변경한다 하더라도 PK 값이 바뀔 가능성 농후 근데 상관없지 어차피 update하면 바꾸면되는ㄴ데
     @DeleteMapping("/{fileId}")
     @Auth(authType = AuthType.USER)
     public ResponseEntity<Void> delete(@PathVariable Long fileId, HttpServletRequest request) {
         fileService.delete(fileId, request);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
-    // PreSigned URL
+
+    // TODO: PreSigned URL
+    @PostMapping("/{bucketName}")
+    public ResponseEntity<CreateFileListResponse> preSignedUpload(
+            @PathVariable String bucketName,
+            @RequestParam("credential") String credential,
+            @ModelAttribute MultipartFile[] files
+    ) {
+        // bucketName을 꼭 넣어야하는가?
+        // 아니면 PreSigned URL을 따로 파고 body에 넣어야하나?
+        // bucketName이랑 credential을 같이 넣어야하나? claims에 다 들어가있는데?
+        // 그냥 검증용으로 둘다 넣기? <- 이게 괜찮은거같은데?
+        return ResponseEntity.status(HttpStatus.CREATED)
+                            .body(fileService.preSignedUpload(bucketName, credential, files));
+    }
 }
