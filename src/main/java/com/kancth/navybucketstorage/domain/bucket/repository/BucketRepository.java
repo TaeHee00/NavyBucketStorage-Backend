f8b19084c31d7fa33e52c262f35470fd18e634b1
Tae Hee Jang <kancth03@gmail.com>
2024-12-02 22:21:51 +0900

    feat: bucket owner 확인을 위한 Query method 생성

--- src/main/java/com/kancth/navybucketstorage/domain/bucket/repository/BucketRepository.java
+++ src/main/java/com/kancth/navybucketstorage/domain/bucket/repository/BucketRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BucketRepository extends JpaRepository<Bucket, Long> {
     List<Bucket> findAllByOwner(User owner);
+
+    Optional<Bucket> findByNameAndOwner_Id(String name, Long id);
 }
